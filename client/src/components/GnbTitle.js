1c22dfa161f1199981057a599e68198330669e97
lamenking <dnwjddus1506@hanmail.net>
2022-01-01 12:36:21 +0900

    💄 Update styles

--- client/src/components/GnbTitle.js
+++ client/src/components/GnbTitle.js
@@ -7,7 +7,10 @@ export default function GnbTitle() {
     <div className="gnb-title">
       <Link to="/">
         <img src={logo} alt="로고" />
-        <h1>공유하면 커피 맛이 두배 SCDT</h1>
+        <h1>
+          공유하면 커피 맛이 두배 <br />
+          SCDT
+        </h1>
       </Link>
     </div>
   );
